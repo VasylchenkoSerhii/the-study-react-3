8675d618692da7852760629886a1a4f71038d67e
Vasylchenko Serhii <darnki@gmail.com>
2022-10-20 07:34:45 +0300

    fixed

--- src/components/ImageGallery/ImageGalleryItem/ImageGalleryItem.jsx
+++ src/components/ImageGallery/ImageGalleryItem/ImageGalleryItem.jsx
@@ -2,7 +2,7 @@ import React from 'react';
 
 export default function ImageGalleryItem({items}) {
     return (
-        items.map(({ id, webformatURL, largeImageURL }) => (
+        items.map(({ id, webformatURL }) => (
             <li className="ImageGalleryItem" key={id}>
                 <img className="ImageGalleryItem-image" src={webformatURL} />
             </li>
