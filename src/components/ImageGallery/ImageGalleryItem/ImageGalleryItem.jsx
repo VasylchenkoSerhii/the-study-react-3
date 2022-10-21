a0042235c47a3b143e1dd5fc77a8d1e150cb338f
Vasylchenko Serhii <darnki@gmail.com>
2022-10-21 06:19:25 +0300

    +

--- src/components/ImageGallery/ImageGalleryItem/ImageGalleryItem.jsx
+++ src/components/ImageGallery/ImageGalleryItem/ImageGalleryItem.jsx
@@ -2,9 +2,9 @@ import React from 'react';
 
 export default function ImageGalleryItem({items}) {
     return (
-        items.map(({ id, webformatURL }) => (
+        items.map(({ id, webformatURL, tags }) => (
             <li className="ImageGalleryItem" key={id}>
-                <img className="ImageGalleryItem-image" src={webformatURL} />
+                <img className="ImageGalleryItem-image" src={webformatURL} alt={tags} />
             </li>
         ))
     );
